33c0115ecc3eff49306cc8f2b897d8903ad2d96c
Антон Величко <90686860+TonyCrafter@users.noreply.github.com>
2022-01-16 01:21:29 +0300

    добавил файлы

---
+++ js/main.js
@@ -0,0 +1,37 @@
+$(function(){
+
+  $('.hamburger').on('click', function() {
+    $('.menu__list').toggleClass('menu__list--active')
+  });
+
+  $('.heroes__slider-img').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    asNavFor: '.heroes__slider-text',
+    prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="images/arrow-left.png" alt="prev"></button>',
+    nextArrow: '<button type="button" class="slick-btn slick-next"><img src="images/arrow-right.png" alt="next"></button>',
+    responsive: [
+    {
+      breakpoint: 769,
+      settings: {
+        arrows: false
+      }
+    }
+  ]
+  });
+
+  $('.heroes__slider-text').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    asNavFor: '.heroes__slider-img',
+    fade: true,
+    arrows: false
+  });
+
+});
+
+let hamburger = document.querySelector(".hamburger");
+
+hamburger.addEventListener("click", function () {
+  hamburger.classList.toggle("is-active");
+});
