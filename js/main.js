5e5719f5ae3e6d67dcef2806f74a0af5a080de74
Антон Величко <90686860+TonyCrafter@users.noreply.github.com>
2022-01-16 02:10:39 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -9,15 +9,7 @@ $(function(){
     slidesToScroll: 1,
     asNavFor: '.heroes__slider-text',
     prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="images/arrow-left.png" alt="prev"></button>',
-    nextArrow: '<button type="button" class="slick-btn slick-next"><img src="images/arrow-right.png" alt="next"></button>',
-    responsive: [
-    {
-      breakpoint: 769,
-      settings: {
-        arrows: false
-      }
-    }
-  ]
+    nextArrow: '<button type="button" class="slick-btn slick-next"><img src="images/arrow-right.png" alt="next"></button>'
   });
 
   $('.heroes__slider-text').slick({
